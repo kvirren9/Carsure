567e117f959c0161c9fbc582a56a1c907eb22c9b
kvirren <niklas.kvist@hotmail.com>
2026-04-10 13:16:24 +0200

    Add Car and Location model classes

---
+++ Models/Car.cs
@@ -0,0 +1,14 @@
+public class Car
+{
+    public int CarId { get; set; }
+    public required string RegNumber { get; set; }
+    public required string Brand { get; set; }
+    public required string Model { get; set; }
+    public required int Year { get; set; }
+    public required string MileAge { get; set; }
+    public required string FuelType { get; set; }
+    public required string Transmission { get; set; }
+    public decimal Price { get; set; }
+    public required string Description { get; set; }
+
+}
---
+++ Models/Location.cs
@@ -0,0 +1,10 @@
+public class Location
+{
+
+    public int LocationId { get; set; }
+    public required string City { get; set; }
+    public required string Region { get; set; }
+    public required string Country { get; set; }
+
+}
+
