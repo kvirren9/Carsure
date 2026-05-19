7a1d51b2152088e683bdbb65685cae3b47500133
kvirren <niklas.kvist@hotmail.com>
2026-05-19 23:38:58 +0200

    Add City and Region fields to Ad and update database schema via migration

---
+++ Migrations/20260519211248_AddCityRegionToAd.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Carsure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCityRegionToAd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Ads",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Region",
+                table: "Ads",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Ads");
+
+            migrationBuilder.DropColumn(
+                name: "Region",
+                table: "Ads");
+        }
+    }
+}
